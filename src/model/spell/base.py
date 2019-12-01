0d2f2162f8b62e4ba5d2661757f93cdd984dc753
Travis Reyburn <treyburn@gmail.com>
2019-11-30 20:09:47 -0700

    Created abstract baseheal and updated heal

---
+++ src/model/spell/base.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class BaseHeal(ABC):
+
+    def __init__(self):
+        self.cost = 1
+        self.cost_coef = 1.0
+        self.cast = 1.0
+        self.cast_coef = 1.0
+        self.heal_low = 1
+        self.heal_high = 1
+        self.power_coef = 1.0
+        self.spell_coef = 1.0
+
+    # @abstractmethod
+    # def set_spell_coef(self, coef):
+    #     self.spell_coef += coef
--- src/model/spell/heal.py
+++ src/model/spell/heal.py
@@ -1,28 +1,11 @@
-from abc import ABC, abstractmethod
-from priest import BasePriest
+from .base import BaseHeal
+from ..priest import BasePriest
 
 
 class Heal(BasePriest):
     def __init__(self):
         self.rank1 = HealRank1()
         self.rank4 = HealRank4()
-
-
-class BaseHeal(ABC):
-
-    def __init__(self):
-        self.cost = 1
-        self.cost_coef = 1.0
-        self.cast = 1.0
-        self.cast_coef = 1.0
-        self.heal_low = 1
-        self.heal_high = 1
-        self.power_coef = 1.0
-        self.spell_coef = 1.0
-
-    # @abstractmethod
-    # def set_spell_coef(self, coef):
-    #     self.spell_coef += coef
 
 
 class HealRank1(BaseHeal):
@@ -42,10 +25,26 @@ class HealRank2(BaseHeal):
 
     def __init__(self):
         self.cost = 205
-        self.cast_coef = 1.0
+        self.cost_coef = 1.0
         self.cast = 3.0
         self.cast_coef = 1.0
+        self.heal_low = 445
+        self.heal_high = 507
+        self.power_coef = 0.857
+        self.spell_coef = 1.0
 
+
+class HealRank3(BaseHeal):
+
+    def __init__(self):
+        self.cost = 255
+        self.cost_coef = 1.0
+        self.cast = 3.0
+        self.cast_coef = 1.0
+        self.heal_low = 586
+        self.heal_high = 662
+        self.power_coef = 0.857
+        self.spell_coef = 1.0
 
 
 class HealRank4(BaseHeal):
